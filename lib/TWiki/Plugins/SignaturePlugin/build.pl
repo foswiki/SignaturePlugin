76789cfbebbc204337ed832ae461cc2187448d93
CrawfordCurrie <CrawfordCurrie@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-03 18:51:53 +0000

    Item6109: removed unpublished API calls
    git-svn-id: http://svn.foswiki.org/trunk/SignaturePlugin@1153 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/TWiki/Plugins/SignaturePlugin/build.pl
+++ lib/TWiki/Plugins/SignaturePlugin/build.pl
@@ -22,10 +22,10 @@ BEGIN {
     unshift @INC, split( /:/, $ENV{FOSWIKI_LIBS} );
 }
 
-use TWiki::Contrib::Build;
+use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build("SignaturePlugin");
+$build = new Foswiki::Contrib::Build("SignaturePlugin");
 
 # Build the target on the command line, or the default target
 $build->build($build->{target});
